d2ae0114f41e9ef8aea377e28cbe48fd93372728
tigereye440 <84235364+tigereye440@users.noreply.github.com>
2024-07-10 11:57:39 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,37 +8,51 @@ module.exports = function (app) {
 
   app.route('/api/convert').get((req, res) => {
 
+    // Get input from url query
     const input = req.query.input;
+
+    // Instantiate an instance of the ConvertHandler class
     const converter = new ConvertHandler();
 
+    // Define variables
     let initUnit;
     let initNum;
     let errs = [];    
 
     try {
+      // try getting the initNum from input
       initNum = converter.getNum(input);
     } catch (err) {
-      console.log(err.message)
+
+      // console.log(err.message)
+      // if err push to errs 
       errs.push(err);
     }
 
     try {
+      // try getting the initUnit from input
       initUnit = converter.getUnit(input); 
     } catch (err) {
-      console.log(err.message)
+      // console.log(err.message)
+      // if err push to errs 
       errs.push(err);
     }
 
+    // Check if there were errors
     if (errs.length > 1) {
+
+      // Respond with error text
       res.json('invalid number and unit')
     } else if (errs.length == 1) {
       res.json(errs[0].message);
     }
 
+    // Get conversion values
     const returnNum = converter.convert(initNum, initUnit);
     const returnUnit = converter.getReturnUnit(initUnit);
     const returnStr = converter.getString(initNum, initUnit, returnNum, returnUnit);
 
+    // Build response string
     let result = {
       initNum: initNum,
       initUnit: initUnit,
@@ -47,6 +61,7 @@ module.exports = function (app) {
       string: returnStr
     }
     
+    // Return result
     res.json(result)
   })
 
